0badb9b801be2a2cb99174d71eab51c137441000
cyoung43 <55853741+cyoung43@users.noreply.github.com>
2020-06-17 10:06:36 -0600

    Rushing page -- donezo

--- projectjs/src/about.js
+++ projectjs/src/about.js
@@ -12,9 +12,9 @@ function About(props) {
                         </bs.Card.Title>
                         <hr />
                         <bs.Card.Text className='text-center mb-3' style={{fontSize: '20pt'}}>
-                            Have you ever wondered what it is like to shoot in a real NBA game? Well we will simulate that experience for you!
-                            Just enter the given information and we will tell you how likely you are to make a shot! With the time running down
-                            in the fourth quarter will you make the game winning shot? Let's find out!
+                            This app is designed to help BYU coaches in their playmaking decisions. It takes
+                            play by play data and predicts the potential yards gained on a play depending on 
+                            play situations and variables. NOTE: This is still in trial phase and has a lot of work to be considered game-ready!
                         </bs.Card.Text>
                     </bs.Card.Body>
                 </bs.Col>
